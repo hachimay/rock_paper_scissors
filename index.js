5ca9b884a066874a383b0f3fc99555d00eae6de2
hachimay <150228729+hachimay@users.noreply.github.com>
2024-01-31 14:47:34 +0900

    Add function to play single round of player vs. computer

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ function getComputerChoice(computerPlay){
     return choices[Math.floor(Math.random() * 3)];
 }
 console.log(getComputerChoice())
+
+
+function playRound(playerSelection, computerSelection){
+    const choices = ["Rock", "Paper", "Scissors"];
+    return "";
+
+    if (playerSelection === computerSelection){
+        return "It's a tie!";
+
+    }else {
+        
+        switch (choices){
+            case "Rock": (computerSelection === "Scissors") ? "You win! Rock beats scissors." : "You lose! Paper beats rock.";
+            break;
+            case "Paper": (computerSelection === "Rock") ? "You win! Paper beats rock." : "You lose! Scissors beats paper.";
+            break;
+            case "Scissors": (computerSelection === "Paper") ? "You win! Scissors beats paper." : "You lose! Rock beats scissors.";
+            break;
+        }
+    }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+console.log(playerSelection, computerSelection);
