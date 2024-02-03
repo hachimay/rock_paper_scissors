0e673e2a1666b37ada35742334894ce1d78553b3
hachimay <150228729+hachimay@users.noreply.github.com>
2024-02-03 22:49:19 +0900

    Rework playRound function.
    Add new function, checkWinner, and change switch to if...else.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
-function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random() * 3)];
-}
-// console.log(getComputerChoice()) 
+const choices = ["Rock", "Paper", "Scissors"];
 
-function getPlayerChoice(){
-    let validatedInput = false; 
-    while(validatedInput == false){ 
-        const option = prompt("Choose either rock, paper, or scissors"); 
-        if(option== null){ 
-            continue;
-        }
-       
-        const optionInLower = option.toLowerCase(); 
-    
-        if(choices.includes(optionInLower)){
-            validatedInput = true; 
-            return optionInLower; 
-        }
+function getComputerChoice(){
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+// console.log(getComputerChoice()) ** works
+
+
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if( // in favor of player, therefore return "player"
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "paper") || 
+        (playerSelection == "paper" && computerSelection == "rock") 
+    ){
+        return "Player"; 
+    }
+    else {
+        return "Computer"; 
     }
 }
-
 
 function playRound(playerSelection, computerSelection){
     let result = "";
@@ -45,13 +45,54 @@ function playRound(playerSelection, computerSelection){
     return result;
 }
 
+function getPlayerChoice(){
+    let validatedInput = false; 
+    while(validatedInput == false){ 
+        const option = prompt("Choose either rock, paper, or scissors"); 
+        if(option== null){ 
+            continue;
+        }
+       
+        const optionInLower = option.toLowerCase(); 
+    
+        if(choices.includes(optionInLower)){
+            validatedInput = true; 
+            return optionInLower; 
+        }
+    }
+}
 
-const computerSelection = getComputerChoice(); 
-const playerSelection = getPlayerChoice();
-console.log(playRound(playerSelection, computerSelection))
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!") //if we want it to say this before each playRound.
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection)); 
+  
+        if (checkWinner(playerSelection, computerSelection) === "Player"){
+            scorePlayer++; 
+        } else if (checkWinner(playerSelection, computerSelection) === "Computer"){
+            scoreComputer++; 
+        }
+    }
+    console.log("Game Over")
+    if(scorePlayer > scoreComputer){
+        console.log("Player was the winner!");
+    }
+    else if (scorePlayer < scoreComputer){
+        console.log("Computer was the winner!");
+    }
+    else {
+        console.log("We have a tie!")
+    }
+}
 
 
-//Write a new function called playGame(). Use the previous function inside of this one
-//to play a five round game that keeps score and reports a winner or loser at the end.
+// const computerSelection = getComputerChoice(); 
+// const playerSelection = getPlayerChoice();
+// console.log(playRound(playerSelection, computerSelection))
+
 
 
